183d48a451bfd6899aebe7cd6141c64803a52a2a
Olpest22 <olpest2@gmail.com>
2024-11-30 21:12:23 +1000

    1.2

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
-const expressHbs = require("express-handlebars")
+const expressHbs = require("express-handlebars");
 const port = 4000;
 
-app.set('view engine', 'hbs')
-app.use('/resources', express.static(__dirname + '/resources'))
+// Устанавливаем view engine для hbs
+app.set('view engine', 'hbs');
 
+// Раздаём статические файлы из папки 'resources'
+app.use('/resources', express.static(__dirname + '/resources'));
+
+// Убираем возможность загружать изображения, запрещая img-src
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' https://yastatic.net data:;");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'none';");
   next();
 });
 
+// Устанавливаем движок для hbs
 app.engine("hbs", expressHbs.engine({
   layoutsDir: "views/layout",
   defaultLayout: "layout",
   extname: "hbs"
-}))
+}));
 
+// Роут для главной страницы
 app.get("/", function (request, response) {
-
   response.render("body.hbs",{
     title: "НАЗВАНИЕ САЙТА",
   });
 });
 
+// Логирование URL запросов для отладки
 app.use((req, res, next) => {
   console.log(`Request URL: ${req.url}`);
   next();
 });
 
+// Запуск сервера на порту 4000
 app.listen(port, '127.0.0.1', function () {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
